217befb11952e81c26d436f46fd53e8e6689a96d
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-02 09:26:26 +0100

    added the register control

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,7 +19,44 @@ const login = asyncHandler(async(req, res) =>{
         })
 
     } else {
-        res.status(401)
-        throw new Error("The email or password is incorrect.")
+        res.status(401).json({ message : "The email or password is incorrect." });
     }
 })
+
+// Desc     Create user in the DB
+// Route    POST /api/user
+// Access   Private
+const register = asyncHandler(async (req, res) => {
+    const { last_name, first_name, email, password } = req.body;
+
+    if (!email || email === "" || !password || password === "") {
+        return res.status(400).json({ message: "Please fill out all the fields." });
+    }
+
+    // Check if the user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+        return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Create user with a default role of "user"
+    const user = await User.create({
+        first_name,
+        last_name,
+        email,
+        password,
+        role: "user",
+    });
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            last_name: user.last_name,
+            first_name: user.first_name,
+            email: user.email,
+            role: user.role,
+        });
+    } else {
+        res.status(400).json({ message: "An error occurred, please try again." });
+    }
+});
