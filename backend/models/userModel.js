da0c096681b3b9bd3af3edca1f6fe444216fe4d9
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-02 09:05:18 +0100

    added password check and password encryption to the user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,3 +28,20 @@ const userSchema = mongoose.Schema({
     }
 
 }, {timestamps: true});
+
+//compare the password with the one in the DB
+userSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+//encrypt the password
+userSchema.pre('save', async function(next){
+    if (!this.isModified('password')){
+        next()
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+module.exports = mongoose.model('User', userSchema)
