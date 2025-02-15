504da6000df6a2a5f0bc3f311e3bc68bdaffc98e
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-15 11:01:58 +0100

    created the route

--- backend/routes/gtfsRoutes.js
+++ backend/routes/gtfsRoutes.js
@@ -2,3 +2,6 @@ const express = require('express');
 const router = express.Router();
 const gtfsController = require('../controllers/gtfsController');
 
+router.post('/update-gtfs', gtfsController.updateGTFS)
+
+module.exports = router;
