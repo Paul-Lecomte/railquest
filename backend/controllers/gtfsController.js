e256f5368a2e83fbe5e44c63959f6a9420b6ee45
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-15 10:34:46 +0100

    define the path to the file

--- backend/controllers/gtfsController.js
+++ backend/controllers/gtfsController.js
@@ -1,3 +1,5 @@
 const { exec } = require('child_process');  // To execute the gtfsUpdater.js script
 const path = require('path');
 
+// Define the path to your gtfsUpdater.js file
+const gtfsUpdaterPath = path.join(__dirname, '../gtfsUpdater.js');
