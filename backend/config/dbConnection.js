85b321b1411613da6eeccd7b08d4acc139856ee3
Paul <lecomtepaulvd@gmail.com>
2025-02-18 13:25:12 +0100

    trying to set the timout for mongodb

--- backend/config/dbConnection.js
+++ backend/config/dbConnection.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 //we try to connect to mangoDB
 const connectDB = async () =>{
     try{
-        await mongoose.connect(process.env.DATABASE_URI)
+        await mongoose.connect(process.env.DATABASE_URI, {
+            serverSelectionTimeoutMS: 60000, // 30 seconds timeout for server selection
+            socketTimeoutMS: 60000, // 45 seconds timeout for socket
+        });
     } catch (err){
         console.log(err)
     }
