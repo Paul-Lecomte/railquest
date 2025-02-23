af4ad554d9058bb16cceca49df81597574841c34
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-23 10:35:34 +0100

    added the timetable controller

---
+++ backend/controllers/timeTableController.js
@@ -0,0 +1,12 @@
+const asyncHandler = require("express-async-handler");
+const StopTime = require('../models/stopTimesModel');
+
+const getTimeTableForStop = asyncHandler(async (req, res) => {
+    try {
+        const { stop_id } = req.params;
+        const timetable = await StopTime.find({ stop_id }).sort({ arrival_time: 1 });
+        res.status(200).json(timetable);
+    } catch (err){
+        res.status(500).json({message: 'Error fetching time table', error: err});
+    }
+})
