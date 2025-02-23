08e718fb5e7dff931347b23d6f6dc725b4c0c65b
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-23 10:23:44 +0100

    created routes controller

---
+++ backend/controllers/routesController.js
@@ -0,0 +1,15 @@
+const asyncHandler = require('express-async-handler');
+const Route = require('../models/routesModel');
+
+const getAllRoutes = asyncHandler(async (req, res) => {
+    try {
+        const routes = await Route.find();
+        res.status(200).json(routes);
+    } catch (err){
+        res.status(500).json({message:'Error fetching routes', err: err});
+    }
+})
+
+module.export = {
+    getAllRoutes,
+}
