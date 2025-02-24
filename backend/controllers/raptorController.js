898a1df1b5a0d73bff7f882a69c7afb64358bcd6
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-02-24 08:29:56 +0100

    update

--- backend/controllers/raptorController.js
+++ backend/controllers/raptorController.js
@@ -2,15 +2,20 @@ const raptorService = require('../services/raptorService');
 const asyncHandler = require('express-async-handler');
 
 const getFastestRoute = asyncHandler(async (req, res) => {
+    const { origin_id, destination_id, departure_time } = req.query;
+
+    if (!origin_id || !destination_id || !departure_time) {
+        return res.status(400).json({ message: 'origin_id, destination_id, and departure_time are required.' });
+    }
+
     try {
-        const { origin_id, destination_id, departure_time } = req.query;
         const result = await raptorService.findFastestRoute(origin_id, destination_id, departure_time);
         res.status(200).json(result);
     } catch (err) {
-        res.status(500).json({ message: 'Error running RAPTOR algorithm', error: err });
+        res.status(500).json({ message: 'Error running RAPTOR algorithm', error: err.message });
     }
-})
+});
 
 module.exports = {
     getFastestRoute,
-}
+};
